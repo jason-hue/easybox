7416f703a18182cb13f07d9037b952d16bc1d05f
knifefire <2113130664@qq.com>
2024-07-13 15:52:32 +0800

    优化mount命令参数解析

--- src/oe/mount/mount.md
+++ src/oe/mount/mount.md
@@ -2,9 +2,13 @@
 
 ## Usage
 ```
-mount [OPTION]... [FILE]
+mount [-lhV] 
+mount -a [选项] 
+mount [选项] [--source] <源> | [--target] <目录> 
+mount [选项] <源> <目录> 
+mount <操作> <挂载点> [<目标>]```
 ```
 
 ## About
 
-...
+挂载文件系统
--- src/oe/mount/src/mount_common.rs
+++ src/oe/mount/src/mount_common.rs
@@ -1,10 +1,11 @@
 use std::any::Any;
 use std::ffi::OsString;
 use std::io::{Read, Stdin};
-use clap::{crate_version, Arg, Command};
+use clap::{crate_version, Arg, Command, ArgGroup};
 use clap::error::ContextValue::String;
 use uucore::error::UResult;
 use uucore::format_usage;
+use crate::mount_common::options::OPTIONS_SOURCE;
 
 pub static BASE_CMD_PARSE_ERROR: i32 = 1;
 
@@ -145,28 +146,28 @@ impl Config {
             no_canonicalize: options.is_present(options::NO_CANONICALIZE),
             fake: options.is_present(options::FAKE),
             fork: options.is_present(options::FORK),
-            fstab: options.value_of(options::FSTAB).map(OsString::from),
+            fstab: options.value_of_os(options::FSTAB).map(OsString::from),
             internal_only: options.is_present(options::INTERNAL_ONLY),
             show_labels: options.is_present(options::SHOW_LABELS),
             no_mtab: options.is_present(options::NO_MTAB),
-            options_mode: options.value_of(options::OPTIONS_MODE).map(OsString::from),
-            options_source: options.value_of(options::OPTIONS_SOURCE).map(OsString::from),
+            options_mode: options.value_of_os(options::OPTIONS_MODE).map(OsString::from),
+            options_source: options.value_of_os(options::OPTIONS_SOURCE).map(OsString::from),
             options_source_force: options.is_present(options::OPTIONS_SOURCE_FORCE),
-            options: options.value_of(options::OPTIONS).map(OsString::from),
-            test_opts: options.value_of(options::TEST_OPTS).map(OsString::from),
+            options: options.value_of_os(options::OPTIONS).map(OsString::from),
+            test_opts: options.value_of_os(options::TEST_OPTS).map(OsString::from),
             read_only: options.is_present(options::READ_ONLY),
-            types: options.value_of(options::TYPES).map(OsString::from),
-            source: options.value_of(options::SOURCE).map(OsString::from),
-            target: options.value_of(options::TARGET).map(OsString::from),
-            target_prefix: options.value_of(options::TARGET_PREFIX).map(OsString::from),
+            types: options.value_of_os(options::TYPES).map(OsString::from),
+            source: options.value_of_os(options::SOURCE).map(OsString::from),
+            target: options.value_of_os(options::TARGET).or_else(||options.value_of_os("target_positional")).map(OsString::from),
+            target_prefix: options.value_of_os(options::TARGET_PREFIX).map(OsString::from),
             verbose: options.is_present(options::VERBOSE),
             read_write: options.is_present(options::READ_WRITE),
-            namespace: options.value_of(options::NAMESPACE).map(OsString::from),
+            namespace: options.value_of_os(options::NAMESPACE).map(OsString::from),
             help: options.is_present(options::HELP),
             version: options.is_present(options::VERSION),
-            label: options.value_of(options::LABEL).map(OsString::from),
-            uuid: options.value_of(options::UUID).map(OsString::from),
-            device: options.value_of(options::DEVICE).map(OsString::from),
+            label: options.value_of_os(options::LABEL).map(OsString::from),
+            uuid: options.value_of_os(options::UUID).map(OsString::from),
+            device: options.value_of_os(options::DEVICE).map(OsString::from),
             bind: options.is_present(options::BIND),
             move_: options.is_present(options::MOVE),
             rbind: options.is_present(options::RBIND),
@@ -188,8 +189,10 @@ impl Config {
 pub fn parse_mount_cmd_args(args: impl uucore::Args, about: &str, usage: &str) -> UResult<Config> {
     let command = mount_app(about,usage);
     let args_list = args.collect_lossy();
-    Config::from(&command.try_get_matches_from(args_list).unwrap())
-
+    match command.try_get_matches_from(args_list) {
+        Ok(matches) => Config::from(&matches),
+        Err(e) => Err(uucore::error::USimpleError::new(BASE_CMD_PARSE_ERROR,e.to_string()))
+    }
 }
 ///定义命令行应用结构和参数，用uucore简化代码
 pub fn mount_app<'a>(about: &'a str, usage: &'a str) -> Command<'a> {
@@ -199,93 +202,106 @@ pub fn mount_app<'a>(about: &'a str, usage: &'a str) -> Command<'a> {
         .override_usage(format_usage(usage))
         .infer_long_args(true)
         .arg(
+            Arg::new("target_positional").takes_value(true).help("指明挂载点").index(2).allow_invalid_utf8(true)
+        )
+        .arg(
             Arg::new(options::ALL).short('a').long(options::ALL).help("挂载fstab中的所有文件系统")
         ).arg(
-        Arg::new(options::NO_CANONICALIZE).short('c').long(options::NO_CANONICALIZE).help("不对路径规范化")
+            Arg::new(options::NO_CANONICALIZE).short('c').long(options::NO_CANONICALIZE).help("不对路径规范化")
         ).arg(
-        Arg::new(options::FAKE).short('f').long(options::FAKE).help("模拟挂载")
+            Arg::new(options::FAKE).short('f').long(options::FAKE).help("空运行；跳过 mount(2) 系统调用")
         ).arg(
-        Arg::new(options::FORK).short('F').long(options::FORK).help("fork挂载")
+            Arg::new(options::FORK).short('F').long(options::FORK).help("对每个设备禁用 fork(和 -a 选项一起使用)")
         ).arg(
-        Arg::new(options::FSTAB).short('T').long(options::FSTAB).takes_value(true).help("指定fstab文件路径")
+            Arg::new(options::FSTAB).short('T').long(options::FSTAB).takes_value(true).help("/etc/fstab 的替代文件")
         ).arg(
-        Arg::new(options::INTERNAL_ONLY).short('i').long(options::INTERNAL_ONLY).help("仅显示内部挂载点")
+            Arg::new(options::INTERNAL_ONLY).short('i').long(options::INTERNAL_ONLY).help("不调用 mount.<type> 辅助程序")
         ).arg(
-        Arg::new(options::SHOW_LABELS).short('l').long(options::SHOW_LABELS).help("显示标签")
+            Arg::new(options::SHOW_LABELS).short('l').long(options::SHOW_LABELS).help("也显示文件系统标签")
         ).arg(
-        Arg::new(options::NO_MTAB).short('n').long(options::NO_MTAB).help("不更新mtab文件")
+            Arg::new(options::NO_MTAB).short('n').long(options::NO_MTAB).help("不写 /etc/mtab")
         ).arg(
-        Arg::new(options::OPTIONS_MODE).long(options::OPTIONS_MODE).takes_value(true).help("设置挂载选项模式")
+            Arg::new(options::OPTIONS_MODE).long(options::OPTIONS_MODE).takes_value(true).help("what to do with options loaded from fstab")
         ).arg(
-        Arg::new(options::OPTIONS_SOURCE).long(options::OPTIONS_SOURCE).takes_value(true).help("设置挂载选项源")
+            Arg::new(options::OPTIONS_SOURCE).long(options::OPTIONS_SOURCE).takes_value(true).help(" mount options source")
         ).arg(
-        Arg::new(options::OPTIONS_SOURCE_FORCE).long(options::OPTIONS_SOURCE_FORCE).help("强制设置挂载选项源")
+            Arg::new(options::OPTIONS_SOURCE_FORCE).long(options::OPTIONS_SOURCE_FORCE).help("force use of options from fstab/mtab")
         ).arg(
-        Arg::new(options::OPTIONS).short('o').long(options::OPTIONS).takes_value(true).help("设置挂载选项")
+            Arg::new(options::OPTIONS).short('o').long(options::OPTIONS).takes_value(true).help("挂载选项列表，以英文逗号分隔")
         ).arg(
-        Arg::new(options::TEST_OPTS).short('O').long(options::TEST_OPTS).takes_value(true).help("设置测试选项")
+            Arg::new(options::TEST_OPTS).short('O').long(options::TEST_OPTS).takes_value(true).help("限制文件系统集合(和 -a 选项一起使用)")
         ).arg(
-        Arg::new(options::READ_ONLY).short('r').long(options::READ_ONLY).help("只读挂载")
+            Arg::new(options::READ_ONLY).short('r').long(options::READ_ONLY).help("以只读方式挂载文件系统(同 -o ro)")
         ).arg(
-        Arg::new(options::TYPES).short('t').long(options::TYPES).takes_value(true).help("指定文件系统类型")
+            Arg::new(options::TYPES).short('t').long(options::TYPES).takes_value(true).help("限制文件系统类型集合")
         ).arg(
-        Arg::new(options::SOURCE).long(options::SOURCE).takes_value(true).help("指定源")
+            Arg::new(options::SOURCE).long(options::SOURCE).takes_value(true).help("指明源(路径、标签、uuid)")
         ).arg(
-        Arg::new(options::TARGET).long(options::TARGET).takes_value(true).help("指定目标")
+            Arg::new(options::TARGET).long(options::TARGET).takes_value(true).help("指明挂载点")
         ).arg(
-        Arg::new(options::TARGET_PREFIX).long(options::TARGET_PREFIX).takes_value(true).help("指定目标前缀")
+            Arg::new(options::TARGET_PREFIX).long(options::TARGET_PREFIX).takes_value(true).help("specifies path used for all mountpoints")
         ).arg(
-        Arg::new(options::VERBOSE).short('v').long(options::VERBOSE).help("详细输出")
+            Arg::new(options::VERBOSE).short('v').long(options::VERBOSE).help("打印当前进行的操作")
         ).arg(
-        Arg::new(options::READ_WRITE).short('w').long(options::READ_WRITE).help("读写挂载")
+            Arg::new(options::READ_WRITE).short('w').long(options::READ_WRITE).help("以读写方式挂载文件系统(默认)")
         ).arg(
-        Arg::new(options::NAMESPACE).short('N').long(options::NAMESPACE).takes_value(true).help("指定命名空间")
+            Arg::new(options::NAMESPACE).short('N').long(options::NAMESPACE).takes_value(true).help("perform mount in another namespace")
         ).arg(
-        Arg::new(options::HELP).short('h').long(options::HELP).help("显示帮助信息")
+            Arg::new(options::HELP).short('h').long(options::HELP).help("display this help")
         ).arg(
-        Arg::new(options::VERSION).short('V').long(options::VERSION).help("显示版本信息")
+            Arg::new(options::VERSION).short('V').long(options::VERSION).help("display version")
         ).arg(
-        Arg::new(options::LABEL).short('L').long(options::LABEL).takes_value(true).help("指定标签")
+            Arg::new(options::LABEL).short('L').long(options::LABEL).takes_value(true).help("synonym for LABEL=<label>")
         ).arg(
-        Arg::new(options::UUID).short('U').long(options::UUID).takes_value(true).help("指定UUID")
+            Arg::new(options::UUID).short('U').long(options::UUID).takes_value(true).help("synonym for UUID=<uuid>")
         ).arg(
-        Arg::new(options::DEVICE).takes_value(true).help("指定设备")
+            Arg::new(options::DEVICE).takes_value(true).help(" 按路径指定设备").index(1).allow_invalid_utf8(true)
         ).arg(
-        Arg::new(options::BIND).short('B').long(options::BIND).help("绑定挂载")
+            Arg::new(options::BIND).short('B').long(options::BIND).help("挂载其他位置的子树(同 -o bind)")
         ).arg(
-        Arg::new(options::MOVE).short('M').long(options::MOVE).help("移动挂载")
+            Arg::new(options::MOVE).short('M').long(options::MOVE).help("将子树移动到其他位置")
         ).arg(
-        Arg::new(options::RBIND).short('R').long(options::RBIND).help("递归绑定挂载")
+            Arg::new(options::RBIND).short('R').long(options::RBIND).help("挂载其他位置的子树及其包含的所有子挂载(submount)")
         ).arg(
-        Arg::new(options::MAKE_SHARED).long(options::MAKE_SHARED).help("设置共享挂载")
+            Arg::new(options::MAKE_SHARED).long(options::MAKE_SHARED).help("将子树标记为 共享")
         ).arg(
-        Arg::new(options::MAKE_SLAVE).long(options::MAKE_SLAVE).help("设置从属挂载")
+            Arg::new(options::MAKE_SLAVE).long(options::MAKE_SLAVE).help("将子树标记为 从属")
         ).arg(
-        Arg::new(options::MAKE_PRIVATE).long(options::MAKE_PRIVATE).help("设置私有挂载")
+            Arg::new(options::MAKE_PRIVATE).long(options::MAKE_PRIVATE).help("将子树标记为 私有")
         ).arg(
-        Arg::new(options::MAKE_UNBINDABLE).long(options::MAKE_UNBINDABLE).help("设置不可绑定挂载")
+            Arg::new(options::MAKE_UNBINDABLE).long(options::MAKE_UNBINDABLE).help("将子树标记为 不可绑定")
         ).arg(
-        Arg::new(options::MAKE_RSHARED).long(options::MAKE_RSHARED).help("设置递归共享挂载")
+            Arg::new(options::MAKE_RSHARED).long(options::MAKE_RSHARED).help("递归地将整个子树标记为 共享")
         ).arg(
-        Arg::new(options::MAKE_RSLAVE).long(options::MAKE_RSLAVE).help("设置递归从属挂载")
+            Arg::new(options::MAKE_RSLAVE).long(options::MAKE_RSLAVE).help("递归地将整个子树标记为 从属")
         ).arg(
-        Arg::new(options::MAKE_RPRIVATE).long(options::MAKE_RPRIVATE).help("设置递归私有挂载")
+            Arg::new(options::MAKE_RPRIVATE).long(options::MAKE_RPRIVATE).help("递归地将整个子树标记为 私有")
         ).arg(
-        Arg::new(options::MAKE_RUNBINDABLE).long(options::MAKE_RUNBINDABLE).help("设置递归不可绑定挂载")
-        ).trailing_var_arg(true)
+            Arg::new(options::MAKE_RUNBINDABLE).long(options::MAKE_RUNBINDABLE).help("递归地将整个子树标记为 不可绑定")
+        ).group(
+        ArgGroup::new("operation")
+            .args(&[options::BIND, options::MOVE, options::RBIND,options::MAKE_SHARED, options::MAKE_SLAVE, options::MAKE_PRIVATE,
+                options::MAKE_UNBINDABLE, options::MAKE_RSHARED, options::MAKE_RSLAVE, options::MAKE_RPRIVATE, options::MAKE_RUNBINDABLE])
+            .required(false)
+        )
+        .group(
+            ArgGroup::new("source_operation")
+                .args(&[options::LABEL,options::UUID, options::DEVICE,options::SOURCE])
+                .required(false)
+        )
+        .group(
+            ArgGroup::new("read_write_mode")
+                .args(&[options::READ_ONLY, options::READ_WRITE])
+                .required(false)
+        )
+        .group(
+            ArgGroup::new("options_source")
+                .args(&[options::OPTIONS_SOURCE, options::OPTIONS_SOURCE_FORCE])
+                .required(false)
+        )
+        .trailing_var_arg(true)
     
     
     
 }
-// ///根据配置获取输入数据，可以是file，也可以是 stdin
-// pub fn get_input<'a>(config: &Config, stdin_ref: &'a Stdin) -> UResult<Box<dyn Read + 'a>> {
-//
-// }
-// ///输入数据按照配置进行处理、包装或忽略垃圾邮件字符
-// pub fn handle_input<R: Read>(
-//     input: &mut R,
-//     format: Format,
-//     line_wrap: Option<usize>,
-//     ignore_garbage: bool,
-//     decode: bool,
-// ) -> UResult<()> {}
+
